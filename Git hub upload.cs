9fd7605871fe56c7775a5bed25242b3ecfc65a0d
Sirdmu <34243798+Sirdmu@users.noreply.github.com>
2017-12-08 11:25:49 -0500

    Add files via upload

---
+++ Git hub upload.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+namespace Solo_Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}*/
+/*
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 89;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 20;
+            Console.WriteLine("x = {0}; y = {1}", x, y);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName;
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", yourName);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old", age);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints Hello
+            Console.WriteLine("Hello");
+        }
+    }
+}*/
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Some long 
+                comment text     
+            */
+            int x = 42;
+            Console.WriteLine(x);
+        }
+    }
+}
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 4;
+            Console.WriteLine(x - y);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10 / 4;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25 % 7;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4 + 3 * 2;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = (4 + 3) * 2;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 42;
+            x += 2; // equivalent to x = x + 2
+            Console.WriteLine(x);
+
+            x -= 6; // equivalent to x = x - 6
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = ++x;
+            Console.WriteLine(x + " " + y);
+        }
+    }
+}*/
+/*namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = x++;
+            Console.WriteLine(x + " " + y);
+        }
+    }
+}*/
